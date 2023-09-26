9b746f16f9151ffdc895c63d5f76f4b20f1b8df8
Suyash Gaurav <102952185+SuyashGaurav@users.noreply.github.com>
2023-09-26 17:56:51 +0530

    hosted

--- app.py
+++ app.py
@@ -59,4 +59,4 @@ def update(id):
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    app.run(host='0.0.0.0', port=5000, debug=True, threaded=False)
+    app.run(host='0.0.0.0', port=5000, debug=True)
